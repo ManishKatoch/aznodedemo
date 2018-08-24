dd68a7bb61aa3558afed7b90630281047e522fdd
Manish <manish.katoch9@gmail.com>
2018-08-24 19:04:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+var http = require('http');
+const KeyVault = require('azure-keyvault');
+const msRestAzure = require('ms-rest-azure');
+
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+});
+
+
+// For public regions the resource name is Key Vault
+msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then( (credentials) => {
+    const keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+
+    var vaultUri = "https://" + "msazpawankeyvault" + ".vault.azure.net/";
+
+    // We're setting the Secret value here and retrieving the secret value
+    keyVaultClient.setSecret(vaultUri, 'my-secret', 'test-secret-value', {})
+        .then( (kvSecretBundle, httpReq, httpResponse) => {
+            console.log("Secret id: '" + kvSecretBundle.id + "'.");
+            return keyVaultClient.getSecret(vaultUri,'my-secret', '', {});
+        })
+        .then( (bundle) => {
+            console.log("Successfully retrieved 'test-secret'");
+            console.log(bundle);
+        })
+        .catch( (err) => {
+            console.log(err);
+        });
+
+return;
+    // Below code demonstrates how to retrieve a secret value
+
+     keyVaultClient.getSecret(vaultUri, "AppSecret", "").then(function(response){
+
+         if (response == null)
+            {
+                console.log("Error: No secret named "+kvSecretBundle.id+" was found...");
+            }
+            else {
+                console.log("Secret value: " + response +
+                    ".  This is for testing only! Never output a secret " +
+                    "to a response or anywhere else in a real app!");
+            }
+     })
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
